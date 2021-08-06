c963666f92140d66fd6b62a3d8b36397474e49b9
rahqis <rahqi@ad.unc.edu>
2021-08-05 21:18:05 -0400

    Initial Commit

---
+++ Tip Calculator Prework/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tip Calculator Prework
+//
+//  Created by Rahqi T. Sarsour on 8/5/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
